83bde55e086fcda0a8012962fc7e4e5ba2856a24
sayaa0 <sonaatana@gmail.com>
2025-05-21 13:14:27 +0900

    Update_6_ArchiveCalender.py

--- ArchiveCalender.py
+++ ArchiveCalender.py
@@ -106,9 +106,8 @@ if channel_id:
                 if day == 0:
                     st.write(" ")
                 else:
-                    if st.button(f"{day}日", key=f"btn-{day}"):
+                    if st.button(f"{day}日を表示", key=f"btn-{day}"):
                         st.session_state['selected_day'] = day
-                    st.markdown(f"**{day}日**")
                     if day in day_map:
                         for idx, v in enumerate(day_map[day]):
                             thumbnail_url = v['snippet']['thumbnails']['default']['url']
